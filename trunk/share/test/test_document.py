7200a0e5297036a3a1bd7805519080d4e85670a5
zgoda <zgoda@d4e82142-c2f2-0310-978b-c129b45ec390>
2006-05-16 11:39:24 +0000

    Test acompanying document-oriented model classes.

---
+++ trunk/share/test/test_document.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of SiteWorkshop project, site object model implementation.
+#
+#       Copyright: (c) 2005, Jarek Zgoda <jzgoda@o2.pl>
+#
+# SiteWorkshop is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License or, at your opinion,
+# any later version.
+#
+# SiteWorkshop is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SiteWorkshop; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+"""Tests for document-centric model"""
+
+__revision__ = '$Id$'
+
+import testprep
+
+import unittest
+
+class DocumentTest(unittest.TestCase):
+    pass
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
